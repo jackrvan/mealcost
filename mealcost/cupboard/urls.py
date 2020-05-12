8a346a4aef99c093ab5f5eddfb54c0193a417122
Jack <jack.vanier16@outlook.com>
2020-05-11 19:15:17 -0600

    Modify add item to use form

--- mealcost/cupboard/urls.py
+++ mealcost/cupboard/urls.py
@@ -6,7 +6,5 @@ app_name = 'cupboard'
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('create/', views.add_item, name='add_item'),
-    path('create/submit', views.add_item_form, name='add_item_form'),
-    # /cupboard/detail/flour/
     path('detail/<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
--- mealcost/cupboard/views.py
+++ mealcost/cupboard/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse
 from django.views import generic
 
 from .models import Item
+from .forms import AddItemForm
 
 
 class Index(generic.ListView):
@@ -30,22 +31,24 @@ class DetailView(generic.DetailView):
         return context
 
 def add_item(request):
-    return render(request, 'cupboard/add_item.html', context={"categories": [i[1] for i in Item.CATEGORY_CHOICES]})
+    if request.method == "POST":
+        # We are processing our form data
+        form = AddItemForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['item_name']
+            ppc = form.cleaned_data['price_per_cup']
+            ppkg = form.cleaned_data['price_per_kg']
+            ppu = form.cleaned_data['price_per_unit']
+            new_item = Item(item_name=name, price_per_cup=(ppc or None), price_per_kg=(ppkg or None), price_per_unit=(ppu or None))
+            new_item.save()
+            # Go back to cupboard index page
+            return HttpResponseRedirect(reverse('cupboard:index'), {'items_in_cupboard': Item.objects.all()})
+    else:
+        form = AddItemForm()
+        return render(request, 'cupboard/add_item.html', context={"form": form, "categories": [i[1] for i in Item.CATEGORY_CHOICES]})
 
 
 def item_detail(request, item_name):
     item = get_object_or_404(Item, item_name=item_name)
     return render(request, 'cupboard/item_detail.html', {'item': item})
 
-
-def add_item_form(request):
-    """Function to add a new item to our database
-    """
-    name = request.POST['item_name']
-    ppc = request.POST['price_per_cup']
-    ppkg = request.POST['price_per_kg']
-    ppu = request.POST['price_per_unit']
-    new_item = Item(item_name=name, price_per_cup=(ppc or None), price_per_kg=(ppkg or None), price_per_unit=(ppu or None))
-    new_item.save()
-    # Go back to cupboard index page
-    return HttpResponseRedirect(reverse('cupboard:index'), {'items_in_cupboard': Item.objects.all()})
