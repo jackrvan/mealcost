273738b74a106cb2db6cd5c252726ce693828faf
Jack <jack.vanier16@outlook.com>
2020-10-06 20:59:52 -0400

    Add bootstrap.

--- mealcost/mealcost/settings.py
+++ mealcost/mealcost/settings.py
@@ -31,15 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'cupboard.apps.CupboardConfig',
-    'recipe.apps.RecipeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third party
     'taggit',
+    'bootstrap4',
+    # Own
+    'cupboard.apps.CupboardConfig',
+    'recipe.apps.RecipeConfig',
 ]
 
 MIDDLEWARE = [
